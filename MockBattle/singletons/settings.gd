00b6a25fb36b2acaf8ebf9e51eea785c8f81d7d2
upa-r-upa <duekthtjf783@gmail.com>
2023-10-23 19:42:22 +0900

    styles: Remove debug print

--- MockBattle/singletons/settings.gd
+++ MockBattle/singletons/settings.gd
@@ -19,21 +19,13 @@ func get_command_settings() -> Dictionary:
 	
 func update_defense_batch_settings(setting: String):
 	batch_settings.defense = setting
-	print("defense_batch_setting: ")
-	print(setting)
 	
 
 func update_attack_batch_settings(setting: String):
 	batch_settings.attack = setting
-	print("attack_batch_setting: ")
-	print(setting)
 
 func update_defense_command_settings(setting: String):
 	batch_settings.defense = setting
-	print("defense_command_setting: ")
-	print(setting)
 
 func update_attack_command_settings(setting: String):
 	batch_settings.attack = setting
-	print("attack_command_setting: ")
-	print(setting)
