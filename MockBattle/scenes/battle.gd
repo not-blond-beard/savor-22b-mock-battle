f8dd12a87c735c9e1244829eab602d459b084bd6
upa-r-upa <duekthtjf783@gmail.com>
2023-10-23 21:49:26 +0900

    feat: Added scene change feature to the battle scene

---
+++ MockBattle/scenes/battle.gd
@@ -0,0 +1,6 @@
+extends Node2D
+
+
+
+func _on_change_scene_pressed():
+	 get_tree().change_scene("res://scenes/settings.tscn")
--- MockBattle/scenes/settings.gd
+++ MockBattle/scenes/settings.gd
@@ -2,3 +2,13 @@ extends Control
 
 func _on_game_start_pressed():
 	 get_tree().change_scene("res://scenes/battle.tscn")
+
+func _ready():
+	var batch_settings = Settings.get_batch_setting()
+	var command_settings = Settings.get_command_settings()
+	
+	$defensive_character/edit.text = batch_settings.get("defense", "")
+	$attack_character/edit.text = batch_settings.get("attack", "")
+	
+	$defensive_command/edit.text = command_settings.get("defense", "")
+	$attack_command/edit.text = command_settings.get("attack", "")
